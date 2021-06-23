7738bb7e909fcae39985165741979384c8788aba
Mariam Aptsiauri <72245573+mariamapciaurii@users.noreply.github.com>
2021-06-23 16:49:34 +0400

    change path and add address

--- src/App.js
+++ src/App.js
@@ -25,14 +25,14 @@ function App() {
     <BrowserRouter>
     <div className="App">
     <Switch>
-          <Route path="/" exact>
+          <Route path="/Quiz-App-ReactJs" exact>
             <Home
               name={name}
               setName={setName}
               fetchQuestions={fetchQuestions}
             />
           </Route>
-          <Route path="/quiz">
+          <Route path="/Quiz-App-ReactJs/quiz">
             <Quiz
               name={name}
               questions={questions}
@@ -41,7 +41,7 @@ function App() {
               setQuestions={setQuestions}
             />
           </Route>
-          <Route path="/result">
+          <Route path="/Quiz-App-ReactJs/result">
             <Result 
             // name={name} score={score} 
             />
